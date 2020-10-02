5568ac655a87cc90a18d3848a5fd2aaeba099f54
Vivek Kumawat <vivekkumawat360@gmail.com>
2020-10-02 15:14:11 +0530

    updated port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv/config");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 
 // Connecting to MongoDB Atlas DB
 mongoose.connect(process.env.DATABASE_URL, {
