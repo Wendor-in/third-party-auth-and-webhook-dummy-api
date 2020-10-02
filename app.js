a49384e3c66e01c7c515f3a4d5ea7c5aa996f4eb
Vivek Kumawat <vivekkumawat360@gmail.com>
2020-10-02 21:37:06 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 3000;
-const DATABASE_URL =
-  "mongodb+srv://admin:vivek-wendor@cluster0.9clex.mongodb.net/DB?retryWrites=true&w=majority";
-
 // Connecting to MongoDB Atlas DB
-mongoose.connect(DATABASE_URL, {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
